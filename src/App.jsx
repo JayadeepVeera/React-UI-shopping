ea94801cf9872880261f4de47511122428a03eec
jai <jai@example.com>
2024-12-30 19:27:55 +0530

    shopping commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import './App.css'
+import Mainpage from "./shoppingfolder/pages/Mainpage.jsx"
+
+const App=()=>{
+  return(
+    <div>
+     <h1>React Shopping</h1>
+     <Mainpage/>
+     </div>
+   
+  )
+}
+
+export default App;
